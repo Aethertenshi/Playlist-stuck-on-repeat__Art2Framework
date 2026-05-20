384bd9a3e4ddf2397d5732c588457a3d50bb2c8e
Aethertenshi <hasan.ahmadibrahim@gmail.com>
2026-05-20 20:01:54 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ArtFrame;
+
+namespace CoreGame
+{
+    public class Runner
+    {
+        static void Main()
+        {
+            Engine.Run<MainGame>();
+        }
+    }
+
+    public class MainGame : IArt
+    {
+
+    }
+}
